8878dd0b9616cc345eda73e08e79d2321590b518
Karim KHACHOUCH <newcolt2@gmail.com>
2022-11-08 20:09:12 +0100

    first commit

---
+++ src/components/inventory/src/build.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+# $Id: build.sh,v 1.2.4.1 2001/03/14 23:27:21 brydon Exp $
+
+if [ -z "$JAVA_HOME" ]
+then
+JAVACMD=`which java`
+if [ -z "$JAVACMD" ]
+then
+echo "Cannot find JAVA. Please set your PATH."
+exit 1
+fi
+JAVA_BINDIR=`dirname $JAVACMD`
+JAVA_HOME=$JAVA_BINDIR/..
+fi
+
+if [ -z "$J2EE_HOME" ]
+then
+echo "Please set J2EE_HOME."
+exit 1
+fi
+
+JAVACMD=$JAVA_HOME/bin/java
+
+ANT_HOME=../../../lib/ant
+ANT_CLASSPATH=$JAVA_HOME/lib/tools.jar
+ANT_CLASSPATH=$ANT_HOME/lib/ant.jar:$ANT_HOME/lib/xml.jar:$ANT_CLASSPATH
+ANT_CLASSPATH=$J2EE_HOME/lib/j2ee.jar:$ANT_CLASSPATH
+$JAVACMD -classpath $ANT_CLASSPATH -Dant.home=$ANT_HOME -Dj2ee.home=$J2EE_HOME org.apache.tools.ant.Main "$@"
